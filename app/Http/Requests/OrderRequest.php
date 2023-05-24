e5ab7e3612fc3f6b73b69c8dc683039304c880f6
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-05-24 11:37:42 +0200

    translate request validation failing messages

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -31,4 +31,27 @@ class OrderRequest extends FormRequest
             'notes' => 'nullable|string',
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return ['name.required' => 'Der Name ist ein Pflichtfeld.',
+            'email.required' => 'Die E-Mail-Adresse ist ein Pflichtfeld.',
+            'email.email' => 'Die E-Mail-Adresse muss eine gültige E-Mail-Adresse sein.',
+            'email.unique' => 'Die E-Mail-Adresse ist bereits vergeben.',
+            'credit_card_number.required' => 'Die Kreditkartennummer ist ein Pflichtfeld.',
+            'credit_card_number.digits' => 'Die Kreditkartennummer muss 16 Ziffern enthalten.',
+            'credit_card_expiration_date.required' => 'Das Ablaufdatum der Kreditkarte ist ein Pflichtfeld.',
+            'credit_card_expiration_date.size' => 'Das Ablaufdatum der Kreditkarte muss 5 Zeichen lang sein.',
+            'credit_card_expiration_date.regex' => 'Das Ablaufdatum der Kreditkarte muss im Format MM/JJ sein.',
+            'total.required' => 'Der Gesamtbetrag ist ein Pflichtfeld.',
+            'total.numeric' => 'Der Gesamtbetrag muss eine Zahl sein.',
+            'status.required' => 'Der Status ist ein Pflichtfeld.',
+            'status.in' => 'Der Status muss einer der folgenden Werte sein: pending, processing, completed.',
+            'notes.string' => 'Die Notizen müssen eine Zeichenfolge sein.'];
+    }
 }
